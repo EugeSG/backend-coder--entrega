7bc3d2c3605d27bc6c756f2ec723d5b83f8ff1ec
Euge <eugeniagonzalez_97@outlook.com>
2024-05-10 12:11:58 -0300

    All products's endpoints

---
+++ src/manager/product.manager.js
@@ -0,0 +1,116 @@
+import fs from "fs";
+import { v4 as uuidv4 } from "uuid";
+
+export default class ProductManager {
+  constructor(path) {
+    this.path = path;
+  }
+
+  async getProducts() {
+    try {
+      if (fs.existsSync(this.path)) {
+        const products = await fs.promises.readFile(this.path, "utf8");
+        return JSON.parse(products);
+      } else return [];
+    } catch (error) {
+      console.log(error);
+    }
+  };
+
+  async createProduct(product) {
+    try {
+      let expectedProps = [
+        "title",
+        "description",
+        "code",
+        "price",
+        "status",
+        "stock",
+        "category"
+      ];
+
+      //Validate required fields
+      const propsArray = (Object.keys(product));
+      if (!expectedProps.every((i) => (propsArray.includes(i)))){
+        return ["Error", "One or more fields are missing"];
+      }
+        
+
+      //Validate required values
+      if (Object.values(product).includes(""))
+        return ["Error", "One or more fields are empty"];
+
+      const productsFile = await this.getProducts();
+      if(productsFile.length != 0) {
+        const productExist = productsFile.find(prod => prod.code == product.code);
+        if(productExist) {
+          return ["Error", "The field 'Code' is already existing. Please change it and try again"];
+          }
+      }
+      product.id = uuidv4();
+      productsFile.push(product);
+      await fs.promises.writeFile(this.path, JSON.stringify(productsFile));
+      return product;
+
+    } catch(error) {
+      console.log(error);
+    }
+  }
+
+
+  async getProducById(idProduct) {
+    try {
+      const productsFile = await this.getProducts();
+
+      // Find id
+      const product = productsFile.find(prod => prod.id == idProduct);
+      if (!product) return ["Error", "Product Not Found"];
+      else return product;
+
+    } catch(error){
+      console.log(error)
+    }
+    //Get products
+    
+  }
+
+  async updateProduct(obj, id) {
+    try {
+      // Verify if Id Exists
+      let productExist = await this.getProducById(id);
+      if (!productExist) return ["Error", "Product Not Found"];
+
+      //Verify Id in body
+      if(obj.id) return ["Error", "The Id can't be modified"]
+      const productsFile = await this.getProducts();
+      productExist = { ...productExist, ...obj };
+      const newArray = productsFile.filter((u) => u.id !== id);
+      newArray.push(productExist)
+      await fs.promises.writeFile(this.path, JSON.stringify(newArray));
+      return productExist;
+
+    } catch (error) {
+      console.log(error);
+    }
+  }
+
+  async deleteProduct(id) {
+    // Verify if Id Existing
+    const productExist = await this.getProducById(id);
+    if(!productExist) return ["Error", "Product Not Found"];
+
+    const products = await this.getProducts();
+    const newArray = products.filter((u) => u.id !== id);
+    await fs.promises.writeFile(this.path, JSON.stringify(newArray));
+    return productExist;
+  }
+
+  // async deleteFile() {
+  //   try {
+  //     await fs.promises.unlink(this.path);
+  //     console.log("archivo eliminado");
+  //   } catch (error) {
+  //     console.log(error);
+  //   }
+  // }
+}
---
+++ src/routes/product.router.js
@@ -0,0 +1,72 @@
+import { Router } from "express";
+const router = Router();
+
+import ProductManager from "../manager/product.manager.js";
+import { upload } from "../middlewares/multer.js";
+
+const productManager = new ProductManager('./src/data/products.json');
+
+router.get("/", async (req, res) => {
+    try {
+        const products = await productManager.getProducts();
+        res.status(200).json(products);
+    } catch (error) {
+        res.status(500).json({ message: "Server Error: " + error.message });
+    }
+});
+
+router.get("/:pid", async (req, res) => {
+    try {
+        const { pid } = req.params;
+        const product = await productManager.getProducById(pid);
+        if (product[0] == "Error") res.status(404).json({ message: product[1] });
+        else res.status(200).json(product);
+    } catch (error) {
+        res.status(500).json({ message: "Server Error: " + error.message });
+    }
+});
+
+router.post("/", upload.array('thumbnails'), async (req, res) => {
+    try {
+        if (req.files.length == 0) res.status(422).json({ message: "The field thumbnails is necesary" })
+        else {
+            const productBody = { ...req.body };
+            let thumbnails = [];
+            for (const file of req.files) {
+                thumbnails.push(file.path);
+            }
+            productBody.thumbnails = thumbnails;
+            const product = await productManager.createProduct(productBody);
+            if (product[0] == "Error") res.status(422).json({ message: product[1] });
+            else res.status(201).json(product);
+        }
+    } catch (error) {
+        res.status(500).json({ message: error.message });
+    }
+});
+
+router.put("/:idProduct", async (req, res) => {
+    try {
+        const { idProduct } = req.params;
+        const productUpdate = await productManager.updateProduct(req.body, idProduct);
+        if (productUpdate[0] == "Error") res.status(422).json({ message: productUpdate[1] });
+        else res.status(200).json(productUpdate);
+    } catch (error) {
+        res.status(500).json({ message: error.message });
+    }
+});
+
+router.delete("/:idProduct", async (req, res) => {
+    try {
+      const { idProduct } = req.params;
+      const productDelete = await productManager.deleteProduct(idProduct);
+      if (productDelete[0] == "Error" ) res.status(404).json({ message: productDelete[1] });
+      else res.status(200).json({ message: `User id: ${idProduct} deleted successfully` });
+    } catch (error) {
+      res.status(500).json({ msg: error.message });
+    }
+  });
+
+
+
+export default router;
